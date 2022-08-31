b8d2b8ee9d8818fb05ea17fe7a0ea9cef09cce6e
diegok23 <dkogut@gmail.com>
2022-08-31 16:43:01 +0200

    Cambio de endpoints de signup y login

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const app = express();
 const api = require('./api');
-const cors = require('cors');
-require('./configs/dotenv');
+//const cors = require('cors');
 const bodyParser = require('body-parser');
-const client = require('./configs/userDB');
-const user = require('./routes/users');
+require('dotenv').config();
+const PORT = process.env.PORT || 4000;
 
 const myLogger = (req, res, next) => {
   const log = {
@@ -16,19 +15,14 @@ const myLogger = (req, res, next) => {
   next();
 };
 
-client.connect((err) => {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log('Data logging initiated!');
-  }
-});
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(myLogger);
-app.use('/users', user);
-app.use(express.json());
-app.use(cors());
+//app.use(cors());
+
+require('./user/routes/auth.routes')(app);
+require('./user/routes/user.routes')(app);
 
 app.get('/', api.main);
 app.get('/search', api.getRoutesBySearch);
@@ -40,6 +34,5 @@ app.get('/routes/:routeId', api.getRoutesById);
 app.post('/routes/:routeId', api.postFavoriteRoute);
 app.post('/users/:userId', api.postRoute);
 
-const PORT = process.env.PORT || 4000;
 const url = `http://localhost:${PORT}`;
 app.listen(PORT, () => console.log(`Listening on port ${url}`));
