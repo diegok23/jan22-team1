762fb3ac01bbbb7085ba1c4bba1af3f2bc739f89
diegok23 <dkogut@gmail.com>
2022-09-06 15:02:14 +0200

    Cambio de nombres de outputs

--- maqueta-fp/src/components/Feed/CardModelNew/CardMain.jsx
+++ maqueta-fp/src/components/Feed/CardModelNew/CardMain.jsx
@@ -1,20 +1,12 @@
-import React from "react";
-import {
-  Avatar,
-  Box,
-  CardContent,
-  CardHeader,
-  Checkbox,
-  Link,
-  Typography,
-} from "@mui/material";
-import { ButtonPer, CardContainer, CardPer } from "./CardS.style";
-import { Bookmark, BookmarkBorder, Directions } from "@mui/icons-material";
-import profileImg from "../../../img/profile-default.jpg";
-import useUser from "../../../hook/useUser";
-import PopupNoLogged from "./PopupNoLogged";
-import PopupNoLoggedSave from "./PopupNoLoggedSave";
-import ImagesCard from "./ImagesCard";
+import React from 'react';
+import { Avatar, Box, CardContent, CardHeader, Checkbox, Link, Typography } from '@mui/material';
+import { ButtonPer, CardContainer, CardPer } from './CardS.style';
+import { Bookmark, BookmarkBorder, Directions } from '@mui/icons-material';
+import profileImg from '../../../img/profile-default.jpg';
+import useUser from '../../../hook/useUser';
+import PopupNoLogged from './PopupNoLogged';
+import PopupNoLoggedSave from './PopupNoLoggedSave';
+import ImagesCard from './ImagesCard';
 
 const CardMain = ({ routes }) => {
   const { isLogged } = useUser();
@@ -26,8 +18,8 @@ const CardMain = ({ routes }) => {
           <CardHeader
             disableTypography
             sx={{
-              backgroundColor: { xs: "primary.main", md: "secondary.main" },
-              color: "white.main",
+              backgroundColor: { xs: 'primary.main', md: 'secondary.main' },
+              color: 'white.main'
             }}
             avatar={<Avatar src={profileImg}></Avatar>}
             title={
@@ -35,12 +27,12 @@ const CardMain = ({ routes }) => {
                 component="p"
                 variant="p"
                 sx={{
-                  fontSize: { xs: "0.7rem", sm: "0.9rem", md: "1rem" },
-                  fontWeight: "800",
-                  fontFamily: "Raleway",
+                  fontSize: { xs: '0.7rem', sm: '0.9rem', md: '1rem' },
+                  fontWeight: '800',
+                  fontFamily: 'Raleway'
                 }}
               >
-                {item.routename}
+                {item.title}
               </Typography>
             }
             subheader={
@@ -48,9 +40,9 @@ const CardMain = ({ routes }) => {
                 component="p"
                 variant="p"
                 sx={{
-                  color: "white.main",
-                  fontFamily: "Nunito Sans",
-                  fontSize: { xs: "0.6rem", sm: "0.7rem", md: "0.8rem" },
+                  color: 'white.main',
+                  fontFamily: 'Nunito Sans',
+                  fontSize: { xs: '0.6rem', sm: '0.7rem', md: '0.8rem' }
                 }}
               >
                 {item.city}, {item.country}
@@ -58,17 +50,15 @@ const CardMain = ({ routes }) => {
             }
           />
 
-          <ImagesCard/>
+          <ImagesCard />
 
-          <CardContent
-            sx={{ backgroundColor: "#fff", height: "20vh", overflowY: "auto" }}
-          >
+          <CardContent sx={{ backgroundColor: '#fff', height: '20vh', overflowY: 'auto' }}>
             <Typography
               variant="body2"
               color="text.secondary"
               sx={{
-                fontSize: { sm: "15px", md: "15px", lg: "18px" },
-                paddingX: { xs: "0px", sm: "8px", lg: "20px" },
+                fontSize: { sm: '15px', md: '15px', lg: '18px' },
+                paddingX: { xs: '0px', sm: '8px', lg: '20px' }
               }}
             >
               {item.description}
@@ -77,11 +67,11 @@ const CardMain = ({ routes }) => {
 
           <CardContent
             sx={{
-              backgroundColor: "#fff",
-              display: "flex",
-              justifyContent: "space-between",
-              alignItems: "center",
-              paddingX: { lg: "30px" },
+              backgroundColor: '#fff',
+              display: 'flex',
+              justifyContent: 'space-between',
+              alignItems: 'center',
+              paddingX: { lg: '30px' }
             }}
           >
             <Typography
@@ -89,22 +79,22 @@ const CardMain = ({ routes }) => {
               variant="body2"
               color="text.secondary"
               sx={{
-                fontSize: { sm: "15px", md: "15px", lg: "18px" },
-                fontWeight: "700",
+                fontSize: { sm: '15px', md: '15px', lg: '18px' },
+                fontWeight: '700'
               }}
             >
-              Route`s Type:{" "}
+              Route`s Type:{' '}
               <Typography variant="spam" fontStyle="italic">
-                {item.routetype}.
-              </Typography>{" "}
-              <Typography variant="spam">{item.routelength} Km</Typography>
+                {item.type}.
+              </Typography>{' '}
+              <Typography variant="spam">{item.distance} Km</Typography>
             </Typography>
             <Typography
               color="text.secondary"
               variant="body2"
               sx={{
-                fontSize: { sm: "15px", md: "15px", lg: "18px" },
-                fontWeight: "700",
+                fontSize: { sm: '15px', md: '15px', lg: '18px' },
+                fontWeight: '700'
               }}
             >
               {item.created_at}
@@ -112,14 +102,14 @@ const CardMain = ({ routes }) => {
           </CardContent>
 
           <CardContainer disableSpacing>
-            <Box sx={{ display: "flex" }}>
+            <Box sx={{ display: 'flex' }}>
               {isLogged ? (
                 <ButtonPer
                   startIcon={
                     <Checkbox
-                      sx={{ padding: "0px" }}
-                      icon={<BookmarkBorder sx={{ color: "white.main" }} />}
-                      checkedIcon={<Bookmark sx={{ color: "yellow" }} />}
+                      sx={{ padding: '0px' }}
+                      icon={<BookmarkBorder sx={{ color: 'white.main' }} />}
+                      checkedIcon={<Bookmark sx={{ color: 'yellow' }} />}
                     />
                   }
                 >
@@ -130,14 +120,12 @@ const CardMain = ({ routes }) => {
               )}
             </Box>
 
-            <Box sx={{ display: "flex" }}>
+            <Box sx={{ display: 'flex' }}>
               {isLogged ? (
-                <ButtonPer
-                  startIcon={<Directions sx={{ color: "white.main" }} />}
-                >
+                <ButtonPer startIcon={<Directions sx={{ color: 'white.main' }} />}>
                   <Link href={`${item.url}`} target="_blank" rel="noopener">
                     Go to map
-                  </Link>{" "}
+                  </Link>{' '}
                 </ButtonPer>
               ) : (
                 <PopupNoLogged />
