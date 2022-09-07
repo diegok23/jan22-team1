2dffb3d0b9e0db30a4134fc94531ccd8d3beff46
laekenrg <laeken.rosado@gmail.com>
2022-09-07 05:01:30 +0200

    Add functionality to search text field

--- maqueta-fp/src/components/Feed/CardModelNew/CardMain.jsx
+++ maqueta-fp/src/components/Feed/CardModelNew/CardMain.jsx
@@ -1,25 +1,97 @@
-import React from 'react';
-import { Avatar, Box, CardContent, CardHeader, Checkbox, Link, Typography } from '@mui/material';
-import { ButtonPer, CardContainer, CardPer } from './CardS.style';
-import { Bookmark, BookmarkBorder, Directions } from '@mui/icons-material';
-import profileImg from '../../../img/profile-default.jpg';
-import useUser from '../../../hook/useUser';
-import PopupNoLogged from './PopupNoLogged';
-import PopupNoLoggedSave from './PopupNoLoggedSave';
-import ImagesCard from './ImagesCard';
+import React from "react";
+import {
+  Avatar,
+  Box,
+  CardContent,
+  CardHeader,
+  Checkbox,
+  Link,
+  Typography,
+} from "@mui/material";
+import { ButtonPer, CardContainer, CardPer } from "./CardS.style";
+import { Bookmark, BookmarkBorder, Directions } from "@mui/icons-material";
+import profileImg from "../../../img/profile-default.jpg";
+import useUser from "../../../hook/useUser";
+import PopupNoLogged from "./PopupNoLogged";
+import PopupNoLoggedSave from "./PopupNoLoggedSave";
+import ImagesCard from "./ImagesCard";
+import { Search } from "@mui/icons-material";
+import {
+  SearchIconWrapper,
+  SearchPer,
+  StyledInputBase,
+} from "../BannerSection/Banner.style";
+import { useState } from "react";
+import { useEffect } from "react";
 
-const CardMain = ({ routes }) => {
+const CardMain = () => {
   const { isLogged } = useUser();
 
+  const [routes, setRoutes] = useState([]);
+  const [search, setSearch] = useState("");
+
+  const URL = "http://localhost:4000/routes/";
+/*GET ALL ROUTES*/
+  const showRoutes = async () => {
+    const response = await fetch(URL);
+    const data = await response.json();
+    setRoutes(data);
+  };
+
+  useEffect(() => {
+    showRoutes();
+  }, []);
+
+  /*TO GET VALUE OF SEARCH INPUT */
+  const searcher = (e) => {
+    setSearch(e.target.value);
+  };
+  /*TO SEARCH A ROUTE BY TITLE, COUNTRY, CITY OR TYPE*/
+  let resultSearch = [];
+  if (!search) {
+    resultSearch = routes;
+  } else {
+    resultSearch = routes.filter(
+      (dato) =>
+        dato.city.toLowerCase().includes(search.toLowerCase()) ||
+        dato.city.toLowerCase().includes(search.toLowerCase()) ||
+        dato.title.toLowerCase().includes(search.toLowerCase()) ||
+        dato.type.toLowerCase().includes(search.toLowerCase())
+    );
+  }
   return (
     <>
-      {routes.map((item, index) => (
+      <Box position="absolute" top="350px" width="80%">
+        <Box display="flex" justifyContent="center">
+          <Typography
+            variant="h5"
+            color="white.main"
+            mr="15px"
+            fontSize="1.6rem"
+          >
+            Search
+          </Typography>
+          <SearchPer>
+            <SearchIconWrapper>
+              <Search />
+            </SearchIconWrapper>
+            <StyledInputBase
+              value={search}
+              onChange={searcher}
+              placeholder="Search…"
+              inputProps={{ "aria-label": "search" }}
+            />
+          </SearchPer>
+        </Box>
+      </Box>
+        
+      {resultSearch.map((item, index) => (
         <CardPer key={index}>
           <CardHeader
             disableTypography
             sx={{
-              backgroundColor: { xs: 'primary.main', md: 'secondary.main' },
-              color: 'white.main'
+              backgroundColor: { xs: "primary.main", md: "secondary.main" },
+              color: "white.main",
             }}
             avatar={<Avatar src={profileImg}></Avatar>}
             title={
@@ -27,9 +99,9 @@ const CardMain = ({ routes }) => {
                 component="p"
                 variant="p"
                 sx={{
-                  fontSize: { xs: '0.7rem', sm: '0.9rem', md: '1rem' },
-                  fontWeight: '800',
-                  fontFamily: 'Raleway'
+                  fontSize: { xs: "0.7rem", sm: "0.9rem", md: "1rem" },
+                  fontWeight: "800",
+                  fontFamily: "Raleway",
                 }}
               >
                 {item.title}
@@ -40,25 +112,27 @@ const CardMain = ({ routes }) => {
                 component="p"
                 variant="p"
                 sx={{
-                  color: 'white.main',
-                  fontFamily: 'Nunito Sans',
-                  fontSize: { xs: '0.6rem', sm: '0.7rem', md: '0.8rem' }
+                  color: "white.main",
+                  fontFamily: "Nunito Sans",
+                  fontSize: { xs: "0.6rem", sm: "0.7rem", md: "0.8rem" },
                 }}
               >
-                {item.city}, {item.country}
+                {item.city}, {item.country} - by {item.created_by}
               </Typography>
             }
           />
 
           <ImagesCard />
 
-          <CardContent sx={{ backgroundColor: '#fff', height: '20vh', overflowY: 'auto' }}>
+          <CardContent
+            sx={{ backgroundColor: "#fff", height: "20vh", overflowY: "auto" }}
+          >
             <Typography
               variant="body2"
               color="text.secondary"
               sx={{
-                fontSize: { sm: '15px', md: '15px', lg: '18px' },
-                paddingX: { xs: '0px', sm: '8px', lg: '20px' }
+                fontSize: { sm: "15px", md: "15px", lg: "18px" },
+                paddingX: { xs: "0px", sm: "8px", lg: "20px" },
               }}
             >
               {item.description}
@@ -67,11 +141,11 @@ const CardMain = ({ routes }) => {
 
           <CardContent
             sx={{
-              backgroundColor: '#fff',
-              display: 'flex',
-              justifyContent: 'space-between',
-              alignItems: 'center',
-              paddingX: { lg: '30px' }
+              backgroundColor: "#fff",
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center",
+              paddingX: { lg: "30px" },
             }}
           >
             <Typography
@@ -79,22 +153,22 @@ const CardMain = ({ routes }) => {
               variant="body2"
               color="text.secondary"
               sx={{
-                fontSize: { sm: '15px', md: '15px', lg: '18px' },
-                fontWeight: '700'
+                fontSize: { sm: "15px", md: "15px", lg: "18px" },
+                fontWeight: "700",
               }}
             >
-              Route`s Type:{' '}
+              Route`s Type:{" "}
               <Typography variant="spam" fontStyle="italic">
                 {item.type}.
-              </Typography>{' '}
+              </Typography>{" "}
               <Typography variant="spam">{item.distance} Km</Typography>
             </Typography>
             <Typography
               color="text.secondary"
               variant="body2"
               sx={{
-                fontSize: { sm: '15px', md: '15px', lg: '18px' },
-                fontWeight: '700'
+                fontSize: { sm: "15px", md: "15px", lg: "18px" },
+                fontWeight: "700",
               }}
             >
               {item.created_at}
@@ -102,14 +176,14 @@ const CardMain = ({ routes }) => {
           </CardContent>
 
           <CardContainer disableSpacing>
-            <Box sx={{ display: 'flex' }}>
+            <Box sx={{ display: "flex" }}>
               {isLogged ? (
                 <ButtonPer
                   startIcon={
                     <Checkbox
-                      sx={{ padding: '0px' }}
-                      icon={<BookmarkBorder sx={{ color: 'white.main' }} />}
-                      checkedIcon={<Bookmark sx={{ color: 'yellow' }} />}
+                      sx={{ padding: "0px" }}
+                      icon={<BookmarkBorder sx={{ color: "white.main" }} />}
+                      checkedIcon={<Bookmark sx={{ color: "yellow" }} />}
                     />
                   }
                 >
@@ -120,12 +194,14 @@ const CardMain = ({ routes }) => {
               )}
             </Box>
 
-            <Box sx={{ display: 'flex' }}>
+            <Box sx={{ display: "flex" }}>
               {isLogged ? (
-                <ButtonPer startIcon={<Directions sx={{ color: 'white.main' }} />}>
+                <ButtonPer
+                  startIcon={<Directions sx={{ color: "white.main" }} />}
+                >
                   <Link href={`${item.url}`} target="_blank" rel="noopener">
                     Go to map
-                  </Link>{' '}
+                  </Link>{" "}
                 </ButtonPer>
               ) : (
                 <PopupNoLogged />
--- maqueta-fp/src/components/Feed/Feed.jsx
+++ maqueta-fp/src/components/Feed/Feed.jsx
@@ -1,38 +1,17 @@
 import React from "react";
 import { Box } from "@mui/material";
 import CardMain from "./CardModelNew/CardMain";
-import { useState } from "react";
-import { useEffect } from "react";
 
-
-
-  const Feed = () => {
-const URL = "http://localhost:4000/routes/"
-const [routes, setRoutes] = useState ([])
-
-const getDataRoutes = (url => {
-  fetch(url)
-  .then((response) => {
-    return response.json();
-  })
-  .then((data) => {
-     setRoutes(data);
-  });
-})
-
-useEffect (() => {
-  getDataRoutes(URL)
-},[])
-
+const Feed = () => {
   return (
     <Box
       sx={{
-        color: "white.main"
+        color: "white.main",
       }}
       flex={4}
       paddingX={{ md: 5, lg: 10, xl: 10 }}
     >
-      <CardMain routes={routes} />
+      <CardMain />
     </Box>
   );
 };
