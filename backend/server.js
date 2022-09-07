3ee7421b8c3c24d3a42ac446cb4dd42c8b3b8ee9
diegok23 <dkogut@gmail.com>
2022-09-07 02:32:25 +0200

    corregido error update-put

--- backend/server.js
+++ backend/server.js
@@ -34,8 +34,8 @@ app.get('/routes', api.getRoutes);
 app.get('/routes/:routeId', api.getRoutesById);
 app.post('/routes/:routeId', api.postFavoriteRoute);
 app.post('/myroutes/:userId', api.postRoute);
-app.update('/user/desc', api.updateUserDescription);
-app.update('/user/pict', api.updateUserPicture);
+app.put('/user/desc', api.updateUserDescription);
+app.put('/user/pict', api.updateUserPicture);
 
 const url = `http://localhost:${PORT}`;
 app.listen(PORT, () => console.log(`Listening on port ${url}`));
